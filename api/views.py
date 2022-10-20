4eb98f7f30bf81b621fd94edefe6e5f54befeab0
Denis, Antonov <den69ant@yandex.ru>
2022-10-20 08:24:41 +0300

    endpoint POST/shop/ has been updated

--- api/views.py
+++ api/views.py
@@ -8,6 +8,10 @@ from .serializers import CitySerializer, StreetSerializer, ShopSerializer
 
 
 class CityView(views.APIView):
+    """
+    Endpoint: GET/city/
+    Получение всех городов из базы
+    """
     def get(self, request):
         streets = City.objects.all()
         serializer = CitySerializer(streets, many=True)
@@ -15,6 +19,10 @@ class CityView(views.APIView):
 
 
 class StreetsOfCityView(views.APIView):
+    """
+    Endpoint: GET/city/<city_id>/street
+    Получение всех улиц города
+    """
     def get(self, request, city_id):
         streets = Street.objects.filter(city=city_id).all()
         serializer = StreetSerializer(streets, many=True)
@@ -22,9 +30,22 @@ class StreetsOfCityView(views.APIView):
     
     
 class ShopCreationView(views.APIView):
+    """
+    Endpoint: POST/street
+    Создание нового магазина
+    """
     def post(self, request):
         serializer = ShopSerializer(data=request.data)
         if serializer.is_valid():
+            # если города с названием city нет в базе, то мы создаем новый город
+            serializer_city = serializer.validated_data["city"]["city_name"]
+            # city_queryset
+            if not City.objects.filter(city_name=serializer_city):
+                new_city = City.objects.create(city_name=serializer_city)
+            # если улицы с названием street нет в базе, то мы создаем новую улицу
+            serializer_street = serializer.validated_data["street"]["street_name"]
+            if not Street.objects.filter(street_name=serializer_street):
+                Street.objects.create(street_name=serializer_street, city=new_city)
             shop = serializer.save()
             return Response({"shop_id": shop.id, "status": "ok"},
                             status=status.HTTP_201_CREATED)
