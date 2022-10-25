f81123b5a72af417f9323d1667887e59320ae2cb
Denis Antonov <den69ant@yandex.ru>
2022-10-25 12:14:31 +0300

    update tests

--- api/tests.py
+++ api/tests.py
@@ -1,20 +1,34 @@
 from rest_framework.test import APITestCase
-from api.models import City
+from api.models import City, Street
 
 
-class ApiTestClass(APITestCase):
+class BaseTest(APITestCase):
     def setUp(self):
-        City.objects.create(city_name="Москва")
+        city = City.objects.create(city_name="Москва")
+        street = Street.objects.create(street_name="Пушкинская", city=city)
 
+
+class CityTestClass(BaseTest):
     def test_get_city(self):
         url = "http://127.0.0.1:8000/api/city/"
         response = self.client.get(url)
-        city = response.json()['cities'][0]['city_name']
+        cities = response.json()['cities']
+        first_city = cities[0]['city_name']
+        number_of_cities = len(cities)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(city, "Москва")
+        self.assertEqual(first_city, "Москва")
+        self.assertEqual(number_of_cities, 1)
 
-
-
+    def test_get_streets_of_city(self):
+        city_id = City.objects.get(city_name="Москва").id
+        url = f"http://127.0.0.1:8000/api/city/{city_id}/street/"
+        response = self.client.get(url)
+        streets = response.json()['streets']
+        first_street = streets[0]['street_name']
+        number_of_streets = len(streets)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(first_street, "Пушкинская")
+        self.assertEqual(number_of_streets, 1)
 
 
 
